d091fc6a69f4ee8c0441927109daed8eff44bfd0
Julian9162 <103683484+Julian9162@users.noreply.github.com>
2024-11-21 23:14:36 -0800

    Update Employee.java
    Add doc

--- CheckInn/Employee.java
+++ CheckInn/Employee.java
@@ -1,14 +1,25 @@
 package CheckInn;
 
-// Class for employee entity
+/**
+ * Employee --- Employee entity class used to store information about an employee and verify their credentials.
+ * @author      Julian Aguiar
+ */
 public class Employee {
 
     // Global variables
     private String first, last; // First name and last name of employee
     private int employeeID; // Identification Number of employee
-    private String password;
+    private String password; // Employee's password
 
-    // Employee constructor that creates employee object based off existing exmployee data
+    /**
+     * Saves information from employee record into an employee object.
+     * @param employeeID  Identification number of the employee
+     * @param last        Last name of the employee
+     * @param first       First name of the employee
+     * @param password    The password for the employee's login
+     * @exception         none
+     * @return            no return value
+     */
     Employee(int employeeID, String last, String first, String password) {
 
         // Saves first and last name + employee email + employee identification number
@@ -18,40 +29,68 @@ public class Employee {
         this.password = password;
         
     } // End Employee(first, last, email, employeeID) constructor
+    
+    /**
+     * Checks if user login credentials match an employees credentials.
+     * @param ID   Identification number to be checked
+     * @param PW   Password to be checked
+     * @exception  none
+     * @return     True if credentials match. False if not.
+     */    
+    public boolean verify(String ID, String PW) {
 
-    // getFirstName() returns first name of employee
+        @SuppressWarnings("unused")
+
+        // Boolean values store true if there is match in employee credentials
+        boolean IDVerified = false, PWVerified = false;
+
+        // Compare inputted ID number to employee's ID number
+        if (ID.compareTo("" + employeeID) == 0) IDVerified = true;
+
+        // Compare inputted password to employee's password
+        if (PW.compareTo("" + password) == 0) PWVerified = true;
+
+        // Return true if credentials match 
+        if (IDVerified = PWVerified) return true;
+
+        // Return false otherwise
+        else return false;
+
+    } // End verify(ID, PW)
+    /**
+     * Returns the first name of the employee.
+     * @exception  none
+     * @return     First name of the employee
+     */
     public String getFirstName() {
         return first;
     } // End getFirstName()
 
-    // getLastName() returns first name of employee
+    /**
+     * Returns the last name of the employee.
+     * @exception  none
+     * @return     Last name of the employee
+     */
     public String getLastName() {
         return last;
     } // End getLastName()
 
-    // getEmployeeID() returns identification number of employee
+    /**
+     * Returns the ID number of the employee.
+     * @exception  none
+     * @return     ID number of the employee
+     */
     public int getEmployeeID() {
         return employeeID;
     } // End getEmployeeID()
 
-    // getPassword() returns the password of the employee account
+    /**
+     * Returns the password of the employee.
+     * @exception  none
+     * @return     Password of the employee
+     */
     public String getPassword() {
         return password;
     } // End getPassword()
-
-    public boolean verify(String ID, String PW) {
-
-        @SuppressWarnings("unused")
-        boolean IDVerified = false, PWVerified = false;
-
-        if (ID.compareTo("" + employeeID) == 0) IDVerified = true;
-
-        if (PW.compareTo("" + password) == 0) PWVerified = true;
-
-        if (IDVerified = PWVerified) return true;
-
-        else return false;
-
-    }
 
 } // End Employee class
