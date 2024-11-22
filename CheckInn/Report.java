60f9ce4caac330efc6df947214a68f31d53ca016
Julian9162 <103683484+Julian9162@users.noreply.github.com>
2024-11-22 00:36:41 -0800

    Update Report.java
    Added doc

--- CheckInn/Report.java
+++ CheckInn/Report.java
@@ -8,7 +8,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.time.LocalDate;
 
-// Report class saves information about a specific report
+/**
+ * Report --- A report entity that saves a report for a specific date and all records associated with report.
+ * @author    Julian Aguiar
+ */
 public class Report {
 
     // Global variables
@@ -16,32 +19,41 @@ public class Report {
     String fileName, title; // Stores name of report and title of report
     ArrayList<String> record = new ArrayList<String>(); // Array of all events in report
 
-    // Report constructor creates report object. Saves specific report file.
+    /**
+     * Report constructor creates a report object. Saves a specific report file.
+     * @param fileName  The name of the report file
+     * @exception       There is an issue reading from the file.
+     * @return          none
+     */
     public Report(String fileName) {
 
         // Local variables
-        String line; // Stores line being read
-        this.fileName = fileName;
+        String line; // Stores a line from the report
+        this.fileName = fileName; // Stores the name of the file
 
         try {
 
+            // Save the report file
             reportFile = new File(fileName);
             // Reads from report file
             BufferedReader reader = new BufferedReader(new FileReader(reportFile));
 
+            // The report is not empty. Saves the first line as the title
             if ((line = reader.readLine()) != null) title = line;
-            
+
+            // The report is empty.
             else {
 
-                BufferedWriter writer = new BufferedWriter(new FileWriter(reportFile));
+                BufferedWriter writer = new BufferedWriter(new FileWriter(reportFile)); // Writes to new report
+                // Writes title of new report into first line
                 writer.write("CheckInn Report: " + LocalDate.now().getMonthValue() + "/" + LocalDate.now().getDayOfMonth()
-                            + "/" + LocalDate.now().getYear());
-                writer.newLine();
-                reader.close();
-                writer.close();
-                return;
-
-            }
+                                + "/" + LocalDate.now().getYear());
+                writer.newLine(); // Adds new line
+                reader.close(); // Close file reader
+                writer.close(); // Close file writer
+                return; // End method
+ 
+            } // End else
 
             // While loop adds all records to array list
             while ((line = reader.readLine()) != null) record.add(line);
@@ -50,46 +62,55 @@ public class Report {
 
         } // End try
 
-        catch(Exception e) {}
+        // Catch exception
+        catch(Exception e) {System.out.println("I/O Error Report Entity: " + e.getMessage())}
 
     } // End Report constructor
 
-    // addToReport method adds new event to report file and array list
+    /**
+     * Adds a new event to this report. 
+     * @param line  The line to be added to this report
+     * @exception   There is an issue writing to the file.
+     * @return      none
+     */
     public void addToReport(String line) {
 
         try {
 
-            BufferedWriter writer = new BufferedWriter(new FileWriter(reportFile, true));
-            writer.write(line);
-            writer.newLine();
-            writer.close();
+            BufferedWriter writer = new BufferedWriter(new FileWriter(reportFile, true)); // Writes to this report
+            writer.write(line); // Writes new event line to the report
+            writer.newLine(); // Writes a new line
+            writer.close(); // Close writer
+            record.add(line); // Add record to record array
 
-            record.add(line);
-
-        }
+        } // End try
 
         catch(Exception e) {System.out.println("Error in Report object: " + e.getMessage());}
 
-    }
+    } // End addToReport(line)
 
-    // ATTRIBUTE GETTERS
-
+    /**
+     * Retrieves the file of the report
+     * @return  The file of the report
+     */
     public File getReportFile() {
-
         return reportFile;
+    } // End getReportFile()
 
-    }
-
+    /**
+     * Retrieves the file name of the report
+     * @return  The file nameof the report
+     */
     public String getFileName() {
-
         return fileName;
+    } // End getFileName()
 
-    }
-
+    /**
+     * Retrieves the title of the report
+     * @return  The title of the report
+     */
     public String getTitle() {
-
         return title;
-
-    }
+    } // End getTitle()
     
 } // End Report class
