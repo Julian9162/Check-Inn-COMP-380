bbda463fe20b3f42b34d417ffd457f1e96853fa4
Julian Aguiar <julian.aguiar.335@my.csun.edu>
2024-11-30 23:46:57 -0800

    Date class files + incomplete edit

---
+++ CheckInn/Date.java
@@ -0,0 +1,15 @@
+package CheckInn;
+
+import java.time.LocalDate;
+
+public class Date {
+
+    int overlap
+    
+    public Date(String checkInDate, String checkOutDate, String roomType) {
+
+
+
+    }
+
+}
